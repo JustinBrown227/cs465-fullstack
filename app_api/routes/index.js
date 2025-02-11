f5d20da547a45a750211fcd7fed67416ee0e48f2
JustinBrown227 <justinbrown2247@gmail.com>
2025-02-11 04:55:46 -0500

    Module 6 completed baseline

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -7,11 +7,14 @@ const tripsController = require('../controllers/trips');
 // Define route for our trips endpoint
 router
     .route('/trips')
-    .get(tripsController.tripsList); // GET method routes triplist
+    .get(tripsController.tripsList) // GET method routes triplist
+    .post(tripsController.tripsAddTrip); // POST Method Adds a Trip
 
 // GET method routes tripsFindByCode 
+// PUT method routes tripsUpdateTrip
 router
     .route('/trips/:tripCode')
-    .get(tripsController.tripsFindByCode); // GET method routes triplist
+    .get(tripsController.tripsFindByCode) // GET method routes triplist
+    .put(tripsController.tripsUpdateTrip);
     
 module.exports = router;
